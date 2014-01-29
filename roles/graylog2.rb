ae7af97827c5a63ff9057ac7fbee45dc5c787c6b
Niranjan Hanumegowda <niranjanmh@yahoo.com>
2014-01-29 13:23:20 -0800

    create new graylog2 role

---
+++ roles/graylog2.rb
@@ -0,0 +1,11 @@
+name "graylog2"
+description "graylog2 role"
+run_list [
+    "recipe[elasticsearch]", 
+    "recipe[mongodb]"
+    ]
+override_attributes(
+  :elasticsearch => {
+    :version => "0.90.10"
+  }
+)
