6efc3bf589b7236976522d7c9599bf273feb4372
Niranjan Hanumegowda <niranjanmh@yahoo.com>
2014-01-29 13:37:33 -0800

    set java version to 7

--- roles/graylog2.rb
+++ roles/graylog2.rb
@@ -7,10 +7,10 @@ run_list [
 override_attributes(
   :java => {
     :jdk_version => "7"
-  }
+  },
   :elasticsearch => {
     :version => "0.90.10"
-  }
+  },
   :mongodb => {
     :package_version => "2.4.9"
   }
