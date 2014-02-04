a4d61ab1f95b49fc1367a054bf628de0cedb1b59
Niranjan Hanumegowda <niranjanmh@yahoo.com>
2014-02-04 13:27:18 -0800

    gelf handler

---
+++ cookbooks/chef_handler/recipes/gelf.rb
@@ -0,0 +1,22 @@
+log_server = search(:node, "role:graylog2").first
+
+if log_server
+  include_recipe "chef_handler::default"
+
+  gem_package "chef-gelf" do
+    action :nothing
+  end.run_action(:install)
+
+  # Make sure the newly installed Gem is loaded.
+  Gem.clear_paths
+  require 'chef/gelf'
+
+  chef_handler "Chef::GELF::Handler" do
+    source "chef/gelf"
+    arguments({
+      :server => log_server['fqdn']
+    })
+
+    supports :exception => true, :report => true
+  end.run_action(:enable)
+end
